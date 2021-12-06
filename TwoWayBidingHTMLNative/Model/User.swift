9f0d7f042f462713c036b9ed7b4a359cebee0204
cuongvx <cuongvx@mobio.vn>
2021-12-06 17:26:13 +0700

    Two way biding between HTML & Native

---
+++ TwoWayBidingHTMLNative/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  TwoWayBidingHTMLNative
+//
+//  Created by Sun on 06/12/2021.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    var email: String
+    var firstName: String
+    var lastName: String
+}
